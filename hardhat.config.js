8bd14a1c9c1eb999580379aa6b1149c48bcc15e6
Ayoub <ayoubagrebi01@gmail.com>
2023-10-28 01:17:55 +0200

    hardhat_config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+networks: {
+    local: {
+      url: "http://localhost:8545", // local Hardhat node
+    },
+  },
 };
